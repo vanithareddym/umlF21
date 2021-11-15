4be066481bf3f6dbd3d3bcad0d89da4cc1600bda
Dan Lambright <dlambrig@gmail.com>
2021-11-14 21:18:16 -0500

    remove boto

--- extract_mysql_full.py
+++ extract_mysql_full.py
@@ -1,6 +1,5 @@
 import pymysql
 import csv
-import boto3
 import configparser
 
 parser = configparser.ConfigParser()
@@ -37,15 +36,3 @@ fp.close()
 m_cursor.close()
 conn.close()
 
-# load the aws_boto_credentials values
-parser = configparser.ConfigParser()
-parser.read("pipeline.conf")
-access_key = parser.get("aws_boto_credentials", "access_key")
-secret_key = parser.get("aws_boto_credentials", "secret_key")
-bucket_name = parser.get("aws_boto_credentials", "bucket_name")
-
-s3 = boto3.client('s3', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
-
-s3_file = local_filename
-
-s3.upload_file(local_filename, bucket_name, s3_file)
